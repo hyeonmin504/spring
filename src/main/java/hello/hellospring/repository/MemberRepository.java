a1741caabdcd248ef1c5680a65021f6cd63cc3de
hyunmin504 <hyunmin504@naver.com>
2023-11-14 21:48:06 +0900

    day2_testcase

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id); //id를 가져올 때 null을 반환하기보다 optional로 감싸서 반환
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+}
